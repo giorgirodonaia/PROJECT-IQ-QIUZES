564360d1a44c4d2c15179d3ecd08b55ac12be8c1
giorgirodonaia <giorgirodonaia20@gmail.com>
2025-01-08 08:07:00 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const questions = [
+  {
+      question: "Which image shows a cat?",
+      options: [
+          { src: "path/to/dog.jpg", isCorrect: false },
+          { src: "path/to/cat.jpg", isCorrect: true },
+          { src: "path/to/bird.jpg", isCorrect: false },
+          { src: "path/to/fish.jpg", isCorrect: false },
+          { src: "path/to/bird.jpg", isCorrect: false },
+          { src: "path/to/fish.jpg", isCorrect: false },
+
+      ]
+  },
+]
